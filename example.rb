5a99c31876eb9c0aecde9968943fcf6e4ae8ecc7
Paulo Angelo <pa@pauloangelo.com>
2021-03-15 22:52:15 -0300

    save

--- example.rb
+++ example.rb
@@ -12,7 +12,7 @@ gk_conf = { "organization_id" => "",
 gk = GuardianKey.new(gk_conf)
 
 client_ip="1.1.1.1"
-user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36"
+user_agent="Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"
 username="test@test.com"
 useremail=username
 login_failed="0"
